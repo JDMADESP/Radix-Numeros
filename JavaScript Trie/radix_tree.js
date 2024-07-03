db49046d6afc0d01021a18a0e4eae5946aa9d711
JDMADESP <152448238+JDMADESP@users.noreply.github.com>
2024-07-03 09:45:01 -0400

    First draft of tree backend

--- JavaScript Trie/radix_tree.js
+++ JavaScript Trie/radix_tree.js
@@ -4,7 +4,15 @@ class Node{
     constructor(){
         this.children = {};
         this.is_END_of_SERIE = false;
-        this.phone_ranges = {}
+        this.info = [];
+    }
+}
+
+class EndNode{
+    constructor(start_val, end_val, info){
+        this.start_val = start_val;
+        this.end_val = end_val;
+        this.information = info;
     }
 }
 
@@ -14,8 +22,48 @@ class Radix_tree{
     }
 
     insert(numeracion_node) {
-        let NIR = numeracion_node.NIR
-        let SERIE = numeracion_node.SERIE
-        first_six_numbers = NIR + SERIE
+        let NIR = numeracion_node.NIR;
+        let SERIE = numeracion_node.SERIE;
+        first_six_numbers = NIR + SERIE;
+        let curr_node = this.root;
+        for (const num in first_six_numbers){
+            if (!curr_node.children[num]){
+                curr_node.children[num] = new Node();
+            }
+            curr_node = curr_node.children[num];
+        }
+        let start_val = Number(numeracion_node.NUM_INICIAL);
+        let end_val = Number(numeracion_node.NUM_FINAL);
+        let information = numeracion_node;
+        let newEndNode = new EndNode(start_val, end_val, information);
+        curr_node.info.push(newEndNode);
+    }
+
+    search(num) {
+        if (num.length != 10){
+            return "You did not enter a valid phone number";
+        }
+        if (isNaN(num)){
+            return "You did not enter a valid phone number";
+        }
+        let first_six = num.slice(0, 6);
+        let last_four = num.slice(6, 10);
+        curr_node = self.root;
+        for (val in first_six){
+            if (!curr_node.children[val]){
+                return "Phone Number not in Database";
+            }
+            curr_node = curr_node.children[val];
+        }
+        if (curr_node.is_END_of_SERIE == false){
+            return "Something wrong with the tree";
+        }
+        for (childNode in curr_node.info){
+            if (childNode.start_val <= Number(last_four) && Number(last_four) <= childNode.end_val){
+                let numeracion = childNode.information;
+                return `Razon social: ${numeracion.razon_social}, Tipo Red: ${numeracion.tipo_red}, Poblacion: ${numeracion.poblacion}`
+            }
+        }
+        return "Phone number not in Database"
     }
 }
